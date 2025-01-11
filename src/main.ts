91fad1cad987d144235775556dc64c39e43189d9
Anton Krusin <krusinanton@gmail.com>
2025-01-11 17:33:11 +0300

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ const API_KEY = "AIzaSyDFftHSo0xYRK0PAjD5Eoc_fCOtmWflVHc";
 const CHANNEL_ID = "UCL-HTw4Wfi9Igh9r1CBrrDA";
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 
-// Получаем путь к текущей директории
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Функция для получения ID плейлиста с загруженными видео канала
 async function getUploadsPlaylistId(channelId: string): Promise<string> {
     const url = `${BASE_URL}/channels`;
     const params = {
@@ -33,13 +31,12 @@ async function getUploadsPlaylistId(channelId: string): Promise<string> {
     }
 }
 
-// Функция для получения всех видео из указанного плейлиста
 async function getAllVideosFromPlaylist(playlistId: string): Promise<{ title: string; videoId: string; description: string; publishedAt: string }[]> {
     const url = `${BASE_URL}/playlistItems`;
     const params = {
         part: "snippet",
         playlistId,
-        maxResults: 50, // Максимум 50 элементов за один запрос
+        maxResults: 50,
         key: API_KEY,
     };
 
@@ -68,16 +65,15 @@ async function getAllVideosFromPlaylist(playlistId: string): Promise<{ title: st
     }
 }
 
-// Функция для записи данных в CSV
+
 function writeToCSV(videos: { title: string; videoId: string; description: string; publishedAt: string }[], filename: string) {
     const csvRows = videos.map((video, index) => {
         const url = `https://www.youtube.com/watch?v=${video.videoId}`;
         const description = video.description
-            .replace(/\n/g, '\n') // сохраняем переносы строк
-            .replace(/"/g, '""'); // экранируем кавычки
-        const date = new Date(video.publishedAt).toISOString().split('T')[0]; // Форматируем дату в YYYY-MM-DD
+            .replace(/\n/g, '\n')
+            .replace(/"/g, '""');
+        const date = new Date(video.publishedAt).toISOString().split('T')[0]; 
 
-        // Возвращаем строку для записи, разделяя элементы
         return `${index + 1} ${url}\n${video.title}\n${date}\n${description}`;
     });
 
